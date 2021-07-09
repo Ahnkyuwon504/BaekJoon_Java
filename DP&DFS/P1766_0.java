42b837918cc99ba84785b43039e8b52ddbf3db02
Ahnkyuwon504 <spider504@naver.com>
2021-07-09 14:16:15 +0900

    solved : 문제집

--- DP&DFS/P1766_0.java
+++ DP&DFS/P1766_0.java
@@ -4,14 +4,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.PriorityQueue;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class P1766 {
     
     static ArrayList<Integer>[] adj;
+    static boolean[] visit;
     static StringBuilder sb;
-    static int[] indegree;
     
     @SuppressWarnings("unchecked")
     public static void main(String[] args) throws IOException {
@@ -25,7 +26,7 @@ public class P1766 {
         int M = Integer.parseInt(st.nextToken());
         
         adj = new ArrayList[N + 1];
-        indegree = new int[N + 1];
+        visit = new boolean[N + 1];
         
         for (int i = 0; i <= N; i++ ) {
             adj[i] = new ArrayList<Integer>();
@@ -38,32 +39,39 @@ public class P1766 {
             int v = Integer.parseInt(st.nextToken());
             
             adj[u].add(v);
-            
-            indegree[v]++;
         }
         
-        PriorityQueue<Integer> q = new PriorityQueue<Integer>();
+        Queue<Integer> q = new LinkedList<Integer>();
         
         for (int i = 1; i <= N; i++) {
-            if (indegree[i] == 0) {
+            if (adj[i].size() != 0) {
                 q.offer(i);
             }
         }
         
         while (!q.isEmpty()) {
-            int node = q.poll();
-            sb.append(node).append(" ");
-            
-            for (int i : adj[node]) {
-                indegree[i]--;
-                
-                if (indegree[i] == 0) q.offer(i);
+            int now = q.poll();
+            bfs(now);
+        }
+        
+        for (int i = 1; i <= N; i++) {
+            if (!visit[i]) {
+                sb.append(i).append(" ");
             }
         }
         
         System.out.println(sb);
+
     }
     
-
+    static void bfs(int now) {
+        visit[now] = true;
+        sb.append(now).append(" ");
+        
+        if (adj[now].size() != 0) {
+            int next = adj[now].get(0);
+            if (!visit[next]) bfs(next);
+        }
+    }
 
 }
