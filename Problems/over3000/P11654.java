5a6775459a513e0b435300e2d9cb714eb8d47452
Ahnkyuwon504 <spider504@naver.com>
2021-07-14 23:06:17 +0900

    solved : 아스키 코드

---
+++ Problems/over3000/P11654.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P11654 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char target = br.readLine().charAt(0);
+		
+		System.out.println((int)target);
+
+	}
+
+}
