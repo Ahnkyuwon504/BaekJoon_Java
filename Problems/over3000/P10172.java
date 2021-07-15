ef5ff9d8e8fc853c63d0ca15befba2c22bbee03a
Ahnkyuwon504 <spider504@naver.com>
2021-07-15 11:41:43 +0900

    solved : 개

---
+++ Problems/over3000/P10172.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+public class P10171 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuilder sb = new StringBuilder();
+		sb.append("|\\_/|").append("\n");
+		sb.append("|q p|   /}").append("\n");
+		sb.append("( 0 )\"\"\"\\").append("\n");
+		sb.append("|\"^\"`    |").append("\n");
+		sb.append("||_/=\\\\__|").append("\n");
+		 
+		System.out.println(sb);
+
+	}
+
+}
