dd8f2caebf7cb4e3b19f851b07f192ea3a527916
Ahnkyuwon504 <spider504@naver.com>
2021-07-15 12:53:17 +0900

    solved : A + B - 7

---
+++ Problems/over3000/P11021.java
@@ -0,0 +1,32 @@
+package baekjoon2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P11021 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		int A, B;
+		
+		for (int tc = 1; tc <= T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			A = Integer.parseInt(st.nextToken());
+			B = Integer.parseInt(st.nextToken());
+			
+			sb.append(String.format("Case #%d: %d\n", tc, A + B));
+		
+		}
+		
+		System.out.println(sb);
+
+	}
+
+}
