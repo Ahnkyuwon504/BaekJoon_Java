4a9d2974763d4dd3ccf721e08adf457cbc474916
Ahnkyuwon504 <spider504@naver.com>
2021-07-15 11:39:58 +0900

    solved : 고양이

---
+++ Problems/over3000/P10171.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+public class P10171 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuilder sb = new StringBuilder();
+		sb.append("\\    /\\").append("\n");
+		sb.append(" )  ( ')").append("\n");
+		sb.append("(  /  )").append("\n");
+		sb.append(" \\(__)|");
+		 
+		System.out.println(sb);
+
+	}
+
+}
