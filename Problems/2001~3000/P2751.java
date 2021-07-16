8104ed75ee81913ed37db04ed0921491db493734
Ahnkyuwon504 <spider504@naver.com>
2021-07-16 12:43:18 +0900

    solved : 수 정렬하기 2

---
+++ Problems/2001~3000/P2751.java
@@ -0,0 +1,29 @@
+package baekjoon2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P2750 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+		boolean[] result = new boolean[2000001];
+		
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(br.readLine());
+			
+			result[num + 1000000] = true;
+		}
+		
+		for (int i = 0; i < result.length; i++) {
+			if (result[i]) sb.append(i - 1000000).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+}
