3b3a0b2d99a3b1faf2bb9876ded9a309d29959e6
Ahnkyuwon504 <spider504@naver.com>
2021-07-16 18:19:37 +0900

    solving Template(BufferedReader/StringTokenizer/StringBuilder)

---
+++ Template.java
@@ -0,0 +1,18 @@
+package baekjoon2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Template {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+		
+	}
+}
