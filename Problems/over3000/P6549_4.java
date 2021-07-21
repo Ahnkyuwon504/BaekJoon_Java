8083a9e317af73fe4fcc791b2b184c272552ffac
Ahnkyuwon504 <spider504@naver.com>
2021-07-21 09:52:57 +0900

    solved : 히스토그램에서 가장 큰 직사각형

---
+++ Problems/over3000/P6549_4.java
@@ -0,0 +1,60 @@
+package baekjoon3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class P6549_3 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		while (true) {
+			st = new StringTokenizer(br.readLine());
+			
+			int n = Integer.parseInt(st.nextToken());
+			
+			if (n == 0) break;
+			
+			int[] histogram = new int[n];
+			
+			for (int i = 0; i < n; i++) {
+				histogram[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			
+			Stack<Integer> stack = new Stack<Integer>();
+			long maxArea = 0;
+			
+			for (int i = 0; i < n; i++) {
+				while (!stack.isEmpty() && histogram[stack.peek()] >= histogram[i]) {
+					int height = histogram[stack.pop()];
+					long width = stack.isEmpty()? i : i - (stack.peek() + 1);
+					
+					maxArea = Math.max(maxArea, height * width);
+				}
+				stack.push(i);
+			}
+			
+			while (!stack.isEmpty()) {
+				int height = histogram[stack.pop()];
+				long width = stack.isEmpty()? n : n - (stack.peek() + 1);
+				
+				maxArea = Math.max(maxArea, height * width);
+			}
+			
+			sb.append(maxArea).append("\n");
+			
+					
+		}
+		
+		System.out.println(sb);
+
+	}
+
+}
