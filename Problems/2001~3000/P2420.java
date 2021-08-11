9a6ad2465f98b391a2cdf335290169d061e37dfd
Ahnkyuwon504 <spider504@naver.com>
2021-08-11 20:49:44 +0900

    solved : 사파리월드

---
+++ Problems/2001~3000/P2420.java
@@ -0,0 +1,21 @@
+package baekjoon4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P2420 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		long N = Long.parseLong(st.nextToken());
+		long M = Long.parseLong(st.nextToken());
+		
+		System.out.println(Math.abs(N - M));
+	}
+
+}
