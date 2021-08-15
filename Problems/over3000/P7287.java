0c0fe4f5e18a9ca3e5f2ef7ee1aa85c4dcaa1335
Ahnkyuwon504 <spider504@naver.com>
2021-08-15 19:17:05 +0900

    solved : 등록

---
+++ Problems/over3000/P7287.java
@@ -0,0 +1,12 @@
+package baekjoon4;
+
+public class P7287 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("153");
+		System.out.println("spider504");
+
+	}
+
+}
