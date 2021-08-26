b9eb169aaafe6858d19164008f5bc3d1ef152d33
Ahnkyuwon504 <spider504@naver.com>
2021-08-27 08:53:39 +0900

    solved : 별 찍기 - 6

---
+++ Problems/2001~3000/P2443.java
@@ -0,0 +1,25 @@
+package baekjoon4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P2443 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= 2 * N - i; j++) {
+				System.out.print((i <= j)? "*" : " ");
+			}
+			System.out.println();
+		}
+				
+
+	}
+
+}
