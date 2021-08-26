3cc966f59e249f306475a5d9bd1730c2f488a96c
Ahnkyuwon504 <spider504@naver.com>
2021-08-26 09:35:43 +0900

    solved : 별 찍기 - 8

---
+++ Problems/2001~3000/P2445.java
@@ -0,0 +1,36 @@
+package baekjoon4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P2445 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i = 1; i <= 2 * N - 1; i++) {
+			for (int j = 1; j <= (2 * N); j++) {
+				if (i <= N) {
+					if ((j <= i) || ((2 * N - i) < j)) {
+						System.out.print("*");
+					} else {
+						System.out.print(" ");
+					}
+				} else {
+					if ((j <= (2 * N - i) || i < j)) {
+						System.out.print("*");
+					} else {
+						System.out.print(" ");
+					}
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
