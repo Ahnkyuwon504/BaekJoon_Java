c693e4b1fae45fe34fec7a131f29fac42d2091ec
Ahnkyuwon504 <spider504@naver.com>
2021-08-28 17:21:55 +0900

    solved : 별 찍기 - 12

---
+++ Problems/2001~3000/P2522.java
@@ -0,0 +1,29 @@
+package baekjoon4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P2522 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i = 1; i <= 2 * N - 1; i++) {
+			for (int j = 1; j <= N; j++) {
+				if (i <= N) {
+					System.out.print((j <= N - i)? " " : "*");
+				} else {
+					System.out.print((i - N < j)? "*" : " ");
+				}
+			}
+			System.out.println();
+		}
+				
+
+	}
+	
+
+}
