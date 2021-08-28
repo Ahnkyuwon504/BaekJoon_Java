10dbff370533bc323acd9fda6b9c4d8224b1d63f
Ahnkyuwon504 <spider504@naver.com>
2021-08-29 07:32:33 +0900

    solved : 별 찍기 - 16

---
+++ Problems/over3000/P10991.java
@@ -0,0 +1,29 @@
+package baekjoon4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P10991 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N + i - 1; j++) {
+				if (N  - i + 1 <= j) {
+					System.out.print(((i + j + N) % 2 == 1)? "*" : " ");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+				
+
+	}
+
+}
